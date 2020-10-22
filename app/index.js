3ce4050aac5037fde2aa7fe095f4d4b7a9fc66a9
ZKrakus <ZKrakus@gmail.com>
2020-10-22 15:16:24 -0400

    Initial commit.

---
+++ app/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Popular from "./components/Popular";
+
+// Component
+// State
+// Lifecycle
+// UI
+
+class App extends React.Component {
+  render() {
+    // The below returns JSX which is a syntax extension to javascript
+    // Browsers don't understand JSX. Babel will transform JSX to plain javascript e.g. using createElement().
+    return (
+      <div className="container">
+        {" "}
+        <Popular />{" "}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("app")
+  // React Element
+  // Where to render the element to
+);
