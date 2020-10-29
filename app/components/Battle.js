04c180c696cad483ba8117c92b6b4492414f50bf
ZKrakus <ZKrakus@gmail.com>
2020-10-29 12:46:27 -0400

    Added skeleton for Results.

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -9,6 +9,7 @@ import {
   FaTimesCircle,
 } from "react-icons/fa";
 import PropTypes from "prop-types";
+import Results from "./Results";
 
 function Instructions() {
   return (
@@ -131,6 +132,7 @@ export default class Battle extends React.Component {
     this.state = {
       playerOne: null,
       playerTwo: null,
+      battle: false,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -150,7 +152,11 @@ export default class Battle extends React.Component {
   }
 
   render() {
-    const { playerOne, playerTwo } = this.state;
+    const { playerOne, playerTwo, battle } = this.state;
+
+    if(battle === true){
+      return <Results playerOne={playerOne} playerTwo={playerTwo} />
+    }
 
     return (
       <React.Fragment>
@@ -185,6 +191,12 @@ export default class Battle extends React.Component {
               />
             )}
           </div>
+
+          {playerOne && playerTwo && (
+            <button className="btn dark-btn btn-space" onClick={() = this.setState({battle: true})}>
+              Battle
+            </button>
+          )}
         </div>
       </React.Fragment>
     );
