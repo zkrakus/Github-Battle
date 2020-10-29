1267f895432cedd8dd28064991748458806f3f16
ZKrakus <ZKrakus@gmail.com>
2020-10-29 15:26:30 -0400

    Began rendering battle output.

--- app/components/Results.js
+++ app/components/Results.js
@@ -2,6 +2,37 @@ import React from "react";
 import { battle } from "../utils/api";
 
 export default class Results extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    const { playerOne, playerTwo } = this.props;
+
+    battle([playerOne, playerTwo])
+      .then((players) => {
+        this.setState({
+          winner: players[0],
+          loser: players[1],
+          error: null,
+          loading: false,
+        });
+      })
+      .catch(({ message }) => {
+        this.setState({
+          error: message,
+          loading: false,
+        });
+      });
+  }
+
   render() {
     return (
       <div>
