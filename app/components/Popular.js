d44cd32cdac5d55db14c09cb7d9c635c9285e460
ZKrakus <ZKrakus@gmail.com>
2020-11-03 10:46:27 -0500

    Rendering results with css.

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -153,7 +153,7 @@ export default class Popular extends React.Component {
 
         {this.isLoading() && <p>LOADING</p>}
 
-        {error && <p>{error}</p>}
+        {error && <p className="center-text error">{error}</p>}
 
         {repos[selectedLanguage] && (
           <ReposGrid repos={repos[selectedLanguage]} />
